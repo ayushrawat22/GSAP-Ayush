1843674c159ba8379697ba60d2a38f23da60b262
ayushrawat22 <ayushrawat271@gmail.com>
2026-02-21 21:07:52 +0530

    made section 2 gallery section through scroll trigger and flip

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+gsap.registerPlugin(Flip);
 gsap.registerPlugin(ScrollTrigger);
+
+
 const tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".cards",
@@ -32,11 +35,84 @@ tl.fromTo(".cards__text--main", {
     ease: "power2.out"
 }, 0.3);
 
+//second sectionm gallery section
+const triggerFlipOnScroll = (galleryWrapEl, options) => {
+    let settings = {
+        flip: {
+            absoluteOnLeave: false,
+            absolute: false,
+            scale: true,
+            simple: true,
+        },
+        scrollTrigger: {
+            start: 'center center',
+            end: '+=300%',
+        },
+        stagger: 0
+    };
 
-ScrollSmoother.create({
-    wrapper: "#smooth-wrapper",
-    content: "#smooth-content",
-    smooth: 1.5,
-    effects: true,
-    smoothTouch: 0.1
-});
+
+    const galleryEl = galleryWrapEl.querySelector('.gallery__wrapper');
+    const galleryCaption = galleryEl.querySelector('.gallery__wrapper--caption');
+    const galleryItems = galleryEl.querySelectorAll('.gallery__wrapper--item');
+    const galleryItemsInner = galleryEl.querySelectorAll('.gallery__wrapper--item-main');
+
+    galleryEl.classList.add('gallery__wrapper--switch');
+    const flipstate = Flip.getState([galleryItems, galleryCaption], {
+        props: 'filter, opacity'
+    });
+
+    galleryEl.classList.remove('gallery__wrapper--switch');
+
+    const tlFlip = Flip.to(flipstate, {
+        ease: 'none',
+        absoluteOnLeave: settings.flip.absoluteOnLeave,
+        absolute: settings.flip.absolute,
+        scale: settings.flip.scale,
+        simple: settings.flip.simple,
+        scrollTrigger: {
+            trigger: galleryEl,
+            start: settings.scrollTrigger.start,
+            end: settings.scrollTrigger.end,
+            pin: galleryWrapEl,
+            scrub: true,
+        },
+    });
+
+    if (galleryItemsInner.length) {
+        tlFlip.fromTo(galleryItemsInner, {
+            scale: 2
+        }, {
+            scale: 1,
+            scrollTrigger: {
+                trigger: galleryEl,
+                start: settings.scrollTrigger.start,
+                end: settings.scrollTrigger.end,
+                scrub: true,
+            },
+        }, 0);
+    }
+};
+
+const gallerySection = document.querySelector('.gallery');
+triggerFlipOnScroll(gallerySection, {});
+
+
+
+let lenis;
+
+const initSmoothScrolling = () => {
+    lenis = new Lenis({
+        lerp: 0.1,
+        smoothWheel: true
+    });
+    lenis.on('scroll', () => ScrollTrigger.update());
+
+    const scrollFn = (time) => {
+        lenis.raf(time);
+        requestAnimationFrame(scrollFn);
+    };
+    requestAnimationFrame(scrollFn);
+};
+
+initSmoothScrolling();
